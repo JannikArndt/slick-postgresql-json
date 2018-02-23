eb2373ac9e1905d1bdffd66ac2ed07c1c5365494
Jannik Arndt <jannik@jannikarndt.de>
2018-02-23 17:07:47 +0100

    Added Json column

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import java.time.LocalDateTime
 
 import MyPostgresProfile.api._
+import play.api.libs.json.{JsValue, Json}
 import shapeless.{Generic, HNil}
 import slick.lifted.ProvenShape
 import slickless._
@@ -14,19 +15,38 @@ object Main extends App {
   val table = TableQuery[JsonTable]
   val db: Database = Database.forConfig("local")
 
-  val action = table += JsonRow(0L, Some("Foo"), Some(LocalDateTime.now()))
+  val json: JsValue = Json.parse("""
+{
+  "name" : "Watership Down",
+  "location" : {
+    "lat" : 51.235685,
+    "long" : -1.309197
+  },
+  "residents" : [ {
+    "name" : "Fiver",
+    "age" : 4,
+    "role" : null
+  }, {
+    "name" : "Bigwig",
+    "age" : 6,
+    "role" : "Owsla"
+  } ]
+}
+""")
+
+  val action = table += JsonRow(0L, Some(json), Some(LocalDateTime.now()))
 
   Await.result(db.run(action), 2 seconds)
 
   db.close()
 }
 
-case class JsonRow(id: Long, json: Option[String], created: Option[LocalDateTime])
+case class JsonRow(id: Long, json: Option[JsValue], created: Option[LocalDateTime])
 
 class JsonTable(tag: Tag) extends Table[JsonRow](tag, Some("slick_pg_test"), "jsontable") {
   def id: Rep[Long] = column[Long]("id", O.PrimaryKey, O.AutoInc)
 
-  def json: Rep[Option[String]] = column[Option[String]]("json")
+  def json: Rep[Option[JsValue]] = column[Option[JsValue]]("json")
 
   def created: Rep[Option[LocalDateTime]] = column[Option[LocalDateTime]]("created")
 
